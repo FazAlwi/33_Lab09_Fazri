1bcae674712d2f202799bcf93b324bcfcb7e7e7b
FazAlwi <132458199+FazAlwi@users.noreply.github.com>
2023-10-18 11:00:54 +0800

    Updated Unity Scene

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,11 +6,21 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] private float Speed = 3;
+    public Player player;
+
+    private void Start()
+    {
+        player = GameObject.Find("AngryDuck").GetComponent<Player>();
+    }
 
     void Update()
     {
         if (transform.position.x <= -8)
+        {
+            player.score++;
+            player.scoreText.text = "Score: " + player.score;
             Destroy(gameObject);
+        }            
         else
             transform.Translate(Vector3.right * Time.deltaTime * -Speed);
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody rb;
+
+    public float JumpForce;
+
+    public Text scoreText;
+    [HideInInspector]public float score;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
@@ -15,6 +24,20 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(0, JumpForce, 0);
+        }
+
+        transform.position = new Vector3(0, Mathf.Clamp(transform.position.y, -3.73f, 3.6f), 0);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+
 }
